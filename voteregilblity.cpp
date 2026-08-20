932231751ab283a8b5cf15d249d33f16234c16e8
CHINMAY <chinmayhasda@gmail.com>
2026-08-20 23:53:40 +0530

    program to check the voter eligiblity

---
+++ voteregilblity.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age ;
+
+    cin>>age;
+
+    if(age>= 18){
+        cout<<" eligible for vote ";
+    }
+    else{
+        cout<<"not eligible for vote ";
+    }
+
+    return 0;
+}
