a1a5536233194b4666ef9c6badcc870d61493016
CHINMAY <chinmayhasda@gmail.com>
2026-08-22 19:12:11 +0530

    program to find number of digit in a number

---
+++ count.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+     int n;
+int count=0;
+
+
+     cin>>n;
+     while(n>0){
+       n=n/10;
+       count++;
+     }
+
+     cout<<count;
+
+     return 0;
+
+
+
+}
