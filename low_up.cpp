32360b490ae8a3e536d8ba49cdbb77f6bf72f2cc
CHINMAY <chinmayhasda@gmail.com>
2026-09-11 21:55:57 +0530

    Toggle the case of each character of a string.

---
+++ low_up.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<cctype>
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin,s);
+
+    for(int i=0;i<s.length();i++){
+       if(islower(s[i])){
+        s[i]=toupper(s[i]);
+       }
+       else if(isupper(s[i])){
+        s[i]=tolower(s[i]);
+       }
+    }
+    
+    cout<<s;
+}
